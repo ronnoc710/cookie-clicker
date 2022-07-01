20a4ddd4336fd0621d57021435a641772f8d4d30
ronnoc710 <bluec0927@gmail.com>
2022-07-01 15:38:09 -0400

    removed default src files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import bigCookie from "./Images/cookie.png";
+import "./App.css"
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="game">
+      <div className="section-top">
+        <div className="cookie-container">
+          <button className="big-cookie-btn">
+            <img src={bigCookie} className="big-cookie-img"/>
+          </button>
+        </div>
+      </div>
+      <div className="section-bottom">
+        <div className="upgrades">
+          <button className="upgrade">
 
-export default App;
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}

a965d8a776f090b9cedf74a89bffe436895734a5
ronnoc710 <bluec0927@gmail.com>
2022-07-05 11:55:53 -0400

    saved cookies to localstorage

---
+++ src/Components/Upgrade-cntnr.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Upgrades() {
+
+  return(
+    <div className="upgrade-cntnr">
+      <button className="upgrade">pointer</button>
+      <button className="upgrade">grandma</button>
+      <button className="upgrade">factory</button>
+    </div>
+  )
+}
